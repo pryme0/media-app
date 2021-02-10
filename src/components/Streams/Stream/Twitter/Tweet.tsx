0b6c070781da07440f39bfc70c1930939af48d60
Santos Bright <itzsantosbright15@gmail.com>
2021-02-10 13:33:07 +0100

    fix twitter errors

---
+++ src/components/Streams/Stream/Twitter/Tweet.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useStyles } from "./styles/Tweet.styles";
+
+interface Props {}
+
+const Tweet = (props: Props) => {
+    return <div>Tweet</div>;
+};
+
+export default Tweet;
