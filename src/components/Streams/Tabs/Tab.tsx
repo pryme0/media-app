260bd90a9282eef13681b9039f2c08693ca8e850
Abadaike Collins <abadaikecollins@gmail.com>
2021-02-10 12:41:22 +0100

    add facebook stream nav, styled tabs properly make tabs and tab panel work

--- src/components/Streams/Tabs/Tab.tsx
+++ src/components/Streams/Tabs/Tab.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { makeStyles, Typography, Tab } from '@material-ui/core';
+import { makeStyles, Typography, Tab, Avatar, withStyles, Theme, createStyles, IconButton } from '@material-ui/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FixMeLater } from '../../../types';
+import { Cancel } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+	iconButton: {
+		marginLeft: theme.spacing(1),
+	},
+	profileImage: {
+		marginRight: theme.spacing(1),
+		position: 'relative',
+	},
+	socialIcon: {
+		position: 'absolute',
+		bottom: -8,
+		right: -10,
+	},
+}));
 
 interface IProfileProps {
 	image: string;
@@ -12,25 +28,14 @@ interface IProfileProps {
 }
 
 const ProfileImageIcon = ({ image, alt, icon }: IProfileProps) => {
+	const classes = useStyles();
 	return (
-		<figure className="image">
-			<img src={image} alt={alt} />
-			<FontAwesomeIcon icon={icon} />
+		<figure className={classes.profileImage}>
+			<Avatar src={image} sizes="small" />
+			<FontAwesomeIcon className={classes.socialIcon} icon={icon} />
 		</figure>
 	);
 };
-
-const useStyles = makeStyles((theme) => ({
-	tab: {
-		height: '40px',
-		minWidth: '90px',
-		maxWidth: '120px',
-		display: 'flex-block',
-		// alignItems: "center",
-		backgroundColor: '#D9E7FA',
-		padding: theme.spacing(0, 2),
-	},
-}));
 
 function a11yProps(index: any) {
 	return {
@@ -39,36 +44,65 @@ function a11yProps(index: any) {
 	};
 }
 
-interface IProps {
-	socialAccount: FixMeLater;
+interface IStyledTabProps {
 	className?: string;
+	value: any;
+	label: string | JSX.Element;
+	icon?: JSX.Element;
 }
 
-const TabItem = ({ socialAccount, ...props }: IProps) => {
+export const StyledTab = withStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			minHeight: '55px!important',
+			padding: theme.spacing(0, 2),
+			'& .svg-inline--fa.fa-twitter': { color: '#258AFF !important' },
+			'& .svg-inline--fa.fa-facebook': { color: '#258AFF !important' },
+		},
+	})
+)((props: IStyledTabProps) => <Tab {...props} />);
+
+const Label = ({ label }: { label: string }) => {
+	const classes = useStyles();
+	return (
+		<div>
+			<span>{label}</span>
+			<IconButton className={classes.iconButton}>
+				<FontAwesomeIcon icon={faTimes} />
+			</IconButton>
+		</div>
+	);
+};
+
+interface ITabProps {
+	socialAccount: FixMeLater;
+	className?: string;
+	value: any;
+	icon?: JSX.Element;
+}
+
+const TabItem = ({ socialAccount, ...props }: ITabProps) => {
 	const classes = useStyles();
 	let icon;
-	switch (socialAccount.account.accountType) {
-		case 'twiiter':
+	switch (socialAccount.account.accountType.toUpperCase()) {
+		case 'TWITTER':
 			icon = faTwitter;
 			break;
-		case 'facebook':
+		case 'FACEBOOK':
 			icon = faFacebook;
 			break;
-		case 'instagram':
+		case 'INSTAGRAM':
 			icon = faInstagram;
 			break;
-		case 'linkedIn':
+		case 'LINKEDIN':
 			icon = faLinkedinIn;
 			break;
 		default:
 			break;
 	}
-
 	return (
-		<Tab
-			className={classes.tab}
-			value={socialAccount._id}
-			label={socialAccount.account.userName}
+		<StyledTab
+			label={<Label label={socialAccount.account.userName} />}
 			icon={<ProfileImageIcon image={socialAccount.account.profileImg} icon={icon} />}
 			{...a11yProps(socialAccount._id)}
 			{...props}
