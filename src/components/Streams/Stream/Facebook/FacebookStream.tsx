f7c4aa41cd01453f8494b00e607a48adf39a8eda
Abadaike Collins <abadaikecollins@gmail.com>
2021-02-12 17:52:43 +0100

    setting thing up for facebook streaming

--- src/components/Streams/Stream/Facebook/FacebookStream.tsx
+++ src/components/Streams/Stream/Facebook/FacebookStream.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { FixMeLater } from '../../../../types';
+import FacebookStreamNav from '../../StreamNavs/FacebokStreamNav';
 import { getFacebookPost } from '../../../../services/facebookStream';
 
 interface IProps {
@@ -8,16 +9,18 @@ interface IProps {
 
 const FacebookStream = ({ socialAccount }: IProps) => {
 	let [posts, setPosts] = useState<FixMeLater[]>([]);
+	let [value, setValue] = React.useState<number>(0);
 
 	useEffect(() => {
 		getFacebookPost(socialAccount.accountId).then((res: FixMeLater) => {
-			debugger;
 			setPosts(res.posts.data);
 		});
 	}, []);
 
 	return (
 		<div>
+			<FacebookStreamNav value={value} setValue={setValue} />
+			<h1>Facebook </h1>
 			{posts.map((post: FixMeLater) => (
 				<p>{post.message}</p>
 			))}
