c6332c999e89e1fbb836ee9480fc2e7425eaae86
Santos Bright <itzsantosbright15@gmail.com>
2021-02-17 17:25:48 +0100

    refractor facebook nav panels and tabs

--- src/components/Streams/Stream/Twitter/TwitterStreamPanels.tsx
+++ src/components/Streams/Stream/Twitter/TwitterStreamPanels.tsx
@@ -1,31 +1,5 @@
-import { Box, Typography } from "@material-ui/core";
 import HomeFeed from "./Feeds/HomeFeed";
-
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`vertical-tabpanel-${index}`}
-            aria-labelledby={`vertical-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box p={0}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
+import TabPanel from "../StreamPanel";
 
 interface Props {
     value: any;
