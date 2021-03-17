858c23a5ea30a00f34d5427ab3a432fe0692cbaa
Abadaike Collins <abadaikecollins@gmail.com>
2021-03-17 11:03:38 +0100

    fixing tab bug

--- src/components/Streams/Tabs/TabPanel.tsx
+++ src/components/Streams/Tabs/TabPanel.tsx
@@ -1,30 +1,24 @@
-import React from "react";
-import { Box, Typography } from "@material-ui/core";
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
 
 interface TabPanelProps {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
+	children?: React.ReactNode;
+	index: any;
+	value: any;
 }
 
 function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
+	const { children, value, index, ...other } = props;
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`wrapped-tabpanel-${index}`}
-            aria-labelledby={`wrapped-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box p={0}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
+	return (
+		<div role="tabpanel" hidden={value !== index} id={`wrapped-tabpanel-${index}`} aria-labelledby={`wrapped-tab-${index}`} {...other}>
+			{value === index && (
+				<Box p={0}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
 }
 
 export default TabPanel;
