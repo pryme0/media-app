ddd537b6ea9c074604e35d3eebba00654e99ba85
Abadaike Collins <abadaikecollins@gmail.com>
2021-04-22 17:59:26 +0100

    presisting data for facebook home feed

--- src/components/Streams/Stream/Facebook/Feeds/HomeFeed.tsx
+++ src/components/Streams/Stream/Facebook/Feeds/HomeFeed.tsx
@@ -4,6 +4,7 @@ import { FixMeLater } from '../../../../../types';
 import StreamContainer from '../../../StreamContainer';
 import { likePost, unLikePost } from '../../../../../services/facebookStream';
 import { addError, addSuccess } from 'redux-flash-messages';
+import { updateCacheData } from '../../../../../services/utils';
 import Loader from '../Loader';
 import Post from './Post';
 
@@ -15,60 +16,55 @@ const HomeFeed = ({ socialAccount }: IProps) => {
 	let [posts, setPosts] = useState<FixMeLater[]>([]);
 	let [loading, setLoading] = React.useState<boolean>(true);
 
-	const togglePostLike = (postId: string | number, hasLike: boolean) => {
+	const togglePostLike = async (postId: string | number, hasLike: boolean) => {
 		if (hasLike) {
-			unLikePost(postId)
-				.then((result) => {
-					let newPostsObj = posts.map((post) =>
-						post.id === postId
-							? {
-									...post,
-									likes: {
-										...post.likes,
-										summary: {
-											...post.likes.summary,
-											has_liked: false,
-											total_count: post.likes.summary.total_count - 1,
-										},
+			let result: FixMeLater = await unLikePost(postId);
+			if (!result.error) {
+				let updateFeeds = posts.map((post) =>
+					post.id === postId
+						? {
+								...post,
+								likes: {
+									...post.likes,
+									summary: {
+										...post.likes.summary,
+										has_liked: false,
+										total_count: post.likes.summary.total_count - 1,
 									},
-							  }
-							: post
-					);
-					setPosts(newPostsObj);
-				})
-				.catch((error) => {
-					console.log(error.response);
-				});
+								},
+						  }
+						: post
+				);
+				setPosts(updateFeeds);
+				updateCacheData(socialAccount.accountId, updateFeeds);
+			}
 		} else {
-			likePost(postId)
-				.then((result) => {
-					let newPostsObj = posts.map((post) =>
-						post.id === postId
-							? {
-									...post,
-									likes: {
-										...post.likes,
-										summary: {
-											...post.likes.summary,
-											has_liked: true,
-											total_count: post.likes.summary.total_count + 1,
-										},
+			let result: FixMeLater = await likePost(postId);
+			if (!result.error) {
+				let updateFeeds = posts.map((post) =>
+					post.id === postId
+						? {
+								...post,
+								likes: {
+									...post.likes,
+									summary: {
+										...post.likes.summary,
+										has_liked: true,
+										total_count: post.likes.summary.total_count + 1,
 									},
-							  }
-							: post
-					);
-					setPosts(newPostsObj);
-				})
-				.catch((error) => {
-					console.log(error.response);
-				});
+								},
+						  }
+						: post
+				);
+				setPosts(updateFeeds);
+				updateCacheData(socialAccount.accountId, updateFeeds);
+			}
 		}
 	};
 
 	useEffect(() => {
 		getFacebookPost(socialAccount.accountId).then((res: FixMeLater) => {
 			setLoading(false);
-			if (res.error) console.log('Code ', res.error.error_subcode, 'Message ', res.error.message, 'Type ', res.error.type);
 
 			if (res.posts.error) {
 				if (res.posts.error.code === 190) addError({ text: 'Password of current account changed. Try relinking account..' });
--- src/services/facebookStream.ts
+++ src/services/facebookStream.ts
@@ -1,7 +1,25 @@
 import { apiCall } from './api';
+import LocalBase from 'localbase';
+import { updateCacheData, cacheData } from './utils';
+import { FixMeLater } from '../types';
+
+let db = new LocalBase('buzzroom');
 
 export const getFacebookPost = async (accountId: string | number) => {
-	return await apiCall('get', `/api/oauth/facebook/getPagePosts/${accountId}`);
+	let feed: any[] = [];
+	let cache = await db
+		.collection(accountId)
+		.get()
+		.then((data: FixMeLater) => {
+			feed = data;
+			return data.length;
+		});
+
+	if (cache > 0) return { posts: { data: feed } };
+	else {
+		// cacheData(accountId, 'home_feed', 'get', `/api/oauth/facebook/getPagePosts/${accountId}`);
+		return await apiCall('get', `/api/oauth/facebook/getPagePosts/${accountId}`);
+	}
 };
 
 export const likePost = async (postId: string | number) => {
